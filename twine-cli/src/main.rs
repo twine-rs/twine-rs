40ea887ddcc8f6ddef19ee7956eda2f589dfd69b
Jake Swensen <jake@swensen.io>
2023-10-18 22:03:59 -0500

    feat: cli: initial cli project

---
+++ twine-cli/src/main.rs
@@ -0,0 +1,20 @@
+use log;
+use socket2::{Domain, Protocol, Socket, Type};
+use std::net::{SocketAddr, TcpListener};
+
+fn check_af_inet6() {
+    let socket = match Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::ICMPV6)) {
+        Ok(s) => s,
+        Err(e) => {
+            log::error!("failed to create socket: {e:?}");
+            return;
+        }
+    };
+
+    log::info!("socket: {:?}", socket);
+}
+
+fn main() {
+    env_logger::init();
+    check_af_inet6();
+}
